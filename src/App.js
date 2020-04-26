0ef2ea1ccdc93ad3b940dfa5ecd749248fa961f3
L-Lisa <lisaojeland@gmail.com>
2020-04-26 22:07:40 +0200

    firstEdition

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import {Card} from "library/Card"
+import styled from "styled-components/macro"
+
+const ThinnerCard = styled(Card)`
+width: 400px;
+`
+
 
 export const App = () => {
   return (
     <div>
-     <Card 
+     <ThinnerCard
      coverImage = "https://www.fillmurray.com/500/300/"
-     thumbnailurl= "https://www.fillmurray.com/500/300/"
+     thumbnailurl= "https://www.fillmurray.com/100/100/"
      title="Title Goes Here" 
      secondaryText="Secondary Text goes here.."
+     supportingText="Supporting text goes here!"
       />
+    <ThinnerCard title="Another Title" secondaryText="Secondary again"/>
+    <Card>
+      <h1>Children here!</h1>
+    </Card>
     </div>
   )
 }
--- src/library/Card.js
+++ src/library/Card.js
@@ -1,11 +1,11 @@
 import React from "react"
 import styled from "styled-components/macro"
-
+/* import PropTypes from 'prop-types'; */
 
 
 const Container = styled.div`
 box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),0px 1px 1px 0px rgba(0, 0, 0, 0.14),0px 1px 3px 0px rgba(0,0,0,.12);
-padding: 20px;
+margin-bottom: 20px;
 border-radius: 3px;
 background: white;
 `;
@@ -21,31 +21,51 @@ const Thumbnail = styled.div`
 width: 60px;
 height: 60px;
 border-radius: 50%;
-background-image: url (${(props) => props.url});
+background-image: url(${(props) => props.url});
 background-size:cover;
 margin-right: 7px;
 `;
+
 const TitleBar= styled.div`
 display: flex;
 align-items: center;
 `;
 
 const CoverImage = styled.img`
-
+width: 100%;
 `;
 
-export const Card = ({title, secondaryText, thumbnailurl, coverImage}) => {
+const Content = styled.div`
+padding:20px;
+`;
+
+const ChildrenContent = styled.div`
+border: 1px solid slategrey;
+margin:20px;
+padding:20px;
+`;
+const SupportingText = styled.div`
+width:100%;
+padding:10px;
+`
+
+export const Card = ({title, secondaryText, thumbnailurl, coverImage, children, className, supportingText}) => {
 return(
 
-    <Container>
-         {coverImage && <CoverImage src={coverImage}/>}
+    <Container className= {className}>
         <TitleBar>
-            <Thumbnail url={thumbnailurl}/>
-            <div>
-            {title && <Title>{title}</Title> }
-            {secondaryText &&  <SecondaryText>{secondaryText}</SecondaryText>}
-            </div>
-        </TitleBar>
+               {thumbnailurl && <Thumbnail url={thumbnailurl}/> } 
+                <div>
+                    {title && <Title>{title}</Title> }
+                    {secondaryText &&  <SecondaryText>{secondaryText}</SecondaryText>}
+                </div>
+            </TitleBar>
+         {coverImage && <CoverImage src={coverImage}/>}
+         {supportingText && <SupportingText/>}
+
+        <Content>
+            {children && <ChildrenContent>{children}</ChildrenContent>}
+        </Content>
     </Container>
 )
 }
